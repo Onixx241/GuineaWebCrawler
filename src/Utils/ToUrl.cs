b37af164867ade5b9f40a8a90a59518bf19ee9e9
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2025-05-22 18:02:24 -0400

    moved ToUrl to utils

---
+++ src/Utils/ToUrl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ToUrl() 
+{
+    public static string ReadUrl()
+    {
+        string url;
+        using (StreamReader sr = new StreamReader("Urlhere.txt"))
+        {
+            url = sr.ReadToEnd().TrimEnd('/');
+        }
+
+        return url;
+    }
+}
